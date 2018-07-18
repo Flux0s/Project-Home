745916c4d8a2018c476aca1193ba067a2b8029c0
Owens <Jason.Owens@prometric.com>
2018-07-18 16:24:50 -0400

    Added support for babel stage-3

--- src/client/App.js
+++ src/client/App.js
@@ -4,24 +4,46 @@ import LoginPage from "./routes/LoginPage";
 
 import "./app.scss";
 
+const homePage = "/login";
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      redirectBack: false
+    };
   }
   render() {
+    const { from } = this.props.location.state || {
+      from: { pathname: homePage }
+    };
+
+    if (this.state.redirectBack) return <Redirect to={from} />;
     return (
       <BrowserRouter>
-        <Route path="/login" component={LoginPage} />
-        <Redirect
-          to={{
-            pathname: "/login",
-            state: { from: props.location }
-          }}
-        />
+        <Route path={homePage} component={LoginPage} />
+        <PrivateRoute path="/secret" component={LoginPage} />
       </BrowserRouter>
     );
   }
 }
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  <Route
+    {...rest}
+    render={(props) =>
+      Auth.isAuthenticated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: homePage,
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />;
+};
 
 export default App;
