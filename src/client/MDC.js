8b5847f38fe1de3f107d3ee7f1e45eed46867520
Owens <Jason.Owens@prometric.com>
2018-07-19 13:22:22 -0400

    Login page content complete.

--- src/client/MDC.js
+++ src/client/MDC.js
@@ -1,1 +1,7 @@
 import { MDCRipple } from "@material/ripple";
+import { MDCTextField } from "@material/textfield";
+
+const username = new MDCTextField(document.querySelector(".login-username"));
+const password = new MDCTextField(document.querySelector(".login-password"));
+
+new MDCRipple(document.querySelector(".login-next"));
--- src/client/routes/LoginPage.js
+++ src/client/routes/LoginPage.js
@@ -10,12 +10,46 @@ class LoginPage extends Component {
   }
   render() {
     return (
-      <section className="login">
-        <div>
+      <div className="login">
+        <section className="login-section mdc-card">
           <img src={Logo} className="login-logo" />
           <h1 className="login-title"> Json Home </h1>
-        </div>
-      </section>
+
+          <form>
+            <div className="mdc-text-field mdc-text-field--box login-username">
+              <input
+                type="text"
+                className="mdc-text-field__input"
+                id="username-input"
+                name="username"
+                required
+              />
+              <label className="mdc-floating-label" htmlFor="username-input">
+                Username
+              </label>
+              <div className="mdc-line-ripple" />
+            </div>
+            <div className="mdc-text-field mdc-text-field--box login-password">
+              <input
+                type="password"
+                className="mdc-text-field__input"
+                id="password-input"
+                name="password"
+                required
+              />
+              <label className="mdc-floating-label" htmlFor="password-input">
+                Password
+              </label>
+              <div className="mdc-line-ripple" />
+            </div>
+            <div className="login-button_container">
+              <button className="mdc-button mdc-button--raised login-next">
+                Next
+              </button>
+            </div>
+          </form>
+        </section>
+      </div>
     );
   }
 }
