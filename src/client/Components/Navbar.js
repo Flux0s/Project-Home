0c5fd2ed42c0dd30d2c8697482ff4cf68679cc27
Jason <Flux0s@users.noreply.github.com>
2018-08-29 23:50:20 -0400

    started adding support for navbar

--- src/client/Components/Navbar.js
+++ src/client/Components/Navbar.js
@@ -1,24 +1,55 @@
 import React, { Component } from "react";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+import { withStyles } from '@material-ui/core/styles';
 import Logo from "../img/Logo.png";
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
 class Navbar extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <header className="navbar mdc-toolbar">
-        <div className="navbar-row mdc-toolbar__row">
-          <section className="navbar-section mdc-toolbar__section mdc-toolbar__section--align-start">
-            <a href="/" className="navbar-title-link">
-              <img className="navbar-logo" src={Logo} />
-              <h1 className="navbar-title mdc-toolbar__title">Json Home</h1>
-            </a>
-          </section>
-          <section className="navbar-section mdc-toolbar__section mdc-toolbar__section--align-end">
-            <this.props.logoutButton />
-          </section>
-        </div>
-      </header>
+      <div className={ classes.root }>
+        <AppBar>
+          <Toolbar>
+            {/* <IconButton className={ classes.menuButton } color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton> */}
+            <Typography variant="title" color="inherit" className={ classes.flex }>
+              Json Home
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+      // <header className="navbar mdc-toolbar">
+      //   <div className="navbar-row mdc-toolbar__row">
+      //     <section className="navbar-section mdc-toolbar__section mdc-toolbar__section--align-start">
+      //       <a href="/" className="navbar-title-link">
+      //         <img className="navbar-logo" src={ Logo } />
+      //         <h1 className="navbar-title mdc-toolbar__title">Json Home</h1>
+      //       </a>
+      //     </section>
+      //     <section className="navbar-section mdc-toolbar__section mdc-toolbar__section--align-end">
+      //       <this.props.logoutButton />
+      //     </section>
+      //   </div>
+      // </header>
     );
   }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
--- src/client/routes/HomePage.js
+++ src/client/routes/HomePage.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
 import Socket from "../Components/Socket";
 import Navbar from "../Components/Navbar";
 import Drawer from "../Components/Drawer";
 import { withRouter } from "react-router-dom";
 
+const styles = {
+  classes: {
+
+  }
+}
+
 class HomePage extends Component {
   static docTitle = "Json Home Config";
-
   constructor(props) {
     super(props);
     this.state = {
@@ -16,9 +22,10 @@ class HomePage extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     return (
       <div className="home">
-        <Navbar logoutButton={this.LogoutButton} />
+        <Navbar logoutButton={ this.LogoutButton } />
         <Drawer />
       </div>
     );
@@ -26,13 +33,13 @@ class HomePage extends Component {
 
   LogoutButton = withRouter(({ history }) => (
     <button
-      onClick={() => {
+      onClick={ () => {
         console.log("Sending Logout request...");
         this.state.socket.emit("Log_Out", () => {
           console.log("Logout callback invoked!");
           this.props.logout(() => history.push("/"));
         });
-      }}
+      } }
       className="mdc-button mdc-button--unelevated home-logout"
     >
       Log Out
@@ -40,4 +47,4 @@ class HomePage extends Component {
   ));
 }
 
-export default HomePage;
+export default withStyles(styles)(HomePage);
